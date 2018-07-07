3769be90eb581191a093a7f82294efd3789b8590
Alex Preissler <AlexJPreissler@Outlook.com>
2018-07-07 11:54:44 -0400

    input update

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,10 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+var q = "q=";
+var ingredient = [];
+var index = 0;
+
 $(document).ready(function(){
 
     //push information to Firebase
@@ -30,4 +34,70 @@ $(document).ready(function(){
         console.log(snapshot.val().id);
         console.log(snapshot.val().recipeList);
     });
+});
+
+$("#searchBtn").on("click", function() {
+
+
+
+});
+
+$(document).on("click", ".category-input", function() {
+
+    var category = $(".category-input:checked").val();
+
+    console.log(category);
+
+    var addIngredientBtn = $("<button>");
+    addIngredientBtn.addClass("btn btn-light");
+    addIngredientBtn.attr("id", "add-ingredient");
+    addIngredientBtn.attr("form", "user-input");
+    addIngredientBtn.attr("type", "submit");
+    addIngredientBtn.text("Add");
+
+    if(category == 'i'){
+
+        $("#user-input").append(addIngredientBtn);
+
+        console.log(addIngredientBtn);
+    
+    }else if(category == "r"){
+
+        $("#add-ingredient").remove();
+
+    }else{}
+
+});
+
+$(document).on("click", "#add-ingredient", function(event) {
+
+    event.preventDefault();
+    
+    ingredient[index] = $("#table_filter").val().trim();
+
+    index++;
+    
+    if(ingredient.length == 1){
+
+        q += ingredient;
+
+    }else{
+
+        for(j=1; j<ingredient.length - 2 ; j++){
+
+            q += "," + ingredient[j];
+
+        }
+
+        q += "," + ingredient[ingredient.length - 1];
+
+    }
+
+
+
+    console.log(ingredient);
+    console.log(q);
+
+    $("#table_filter").val("");
+
 });
