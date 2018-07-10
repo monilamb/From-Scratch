b39912226c2f383c1bb5ae3c02cd49815d8e4bbe
adee <adee4990@gmail.com>
2018-07-10 17:47:45 -0400

    Display information from food2fork

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@ var database = firebase.database();
 
 var q = "q=";
 var ingredient = [];
+var searchedRecipeList = [];
 var index = 0;
 var ingredientBtn = false;
 var queryURL = "http://food2fork.com/api/search?key=ac439ce8f238ddbc8d1f8d5d4e74839a&";
@@ -21,19 +22,7 @@ var email = null;
 var password = null;
 
 $(document).ready(function () {
-  
-    // queryURL += "apples";
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response){
-    //     var results = JSON.parse(response);
-    //     console.log(results);
-    //     $.each(results.recipes, function(index){
-    //         console.log(this.title);
-    //     });
-    // });
-
+    
     // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
     //     // Handle Errors here.
     //     var errorCode = error.code;
@@ -72,112 +61,13 @@ $(document).ready(function () {
     // });
 
     //display data base info
-    database.ref().on("child_added", function(snapshot){
-        console.log(snapshot);
-        console.log(snapshot.val().id);
-        console.log(snapshot.val().recipeList);
-    });
+    // database.ref().on("child_added", function(snapshot){
+    //     console.log(snapshot);
+    //     console.log(snapshot.val().id);
+    //     console.log(snapshot.val().recipeList);
+    // });
 });
 
-//Triggers search request, need to connect to ajax call.
-$(document).on("click", "#searchBtn", function(event) {
-
-    event.preventDefault();
-
-    var category = $(".category-input:checked").val();
-
-//if ingredient category selected and input box contains user input, will add to ingredient list if enter is pressed, constructs queryURL in proper format if enter and no input.    
-    if(category == 'i'){
-
-        if($("#table_filter").val() != ""){
-            
-            ingredientAdd();
-        
-        }else{
-            
-            queryURL = queryURL + q;
-
-            console.log("i search: " + queryURL);
-    
-        }
-
-//if recipe search constructs queryURL in proper format. 
-    }else if(category == 'r'){
-
-        
-        var recipe = $("#table_filter").val();
-        console.log(recipe);
-        var queryRecipe = "q=";
-
-        recipe = recipe.split(" ");
-
-        for(var i=0; i<recipe.length; i++){
-
-            if(i == 0){
-
-                queryRecipe += recipe[i];
-
-            }else{
-
-                queryRecipe += ("+" + recipe[i]);
-
-            }
-
-        }
-
-        queryURL = queryURL + queryRecipe;
-        $("#table_filter").val("");
-        
-        console.log(recipe);
-        console.log("r search: " + queryURL);
-    
-    }else{
-
-    };
-
-});
-
-//adds/removes ingredient add button based on selected category.
-$(document).on("click", ".category-input", function() {
-
-    var category = $(".category-input:checked").val();
-
-    console.log(category);
-
-    var addIngredientBtn = $("<button>");
-    addIngredientBtn.addClass("btn btn-light");
-    addIngredientBtn.attr("id", "add-ingredient");
-    addIngredientBtn.attr("form", "user-input");
-    addIngredientBtn.attr("type", "submit");
-    addIngredientBtn.text("Add");
-
-    if(category == 'i' && ingredientBtn == false){
-
-        ingredientBtn = true;
-        $("#dropdown-button").append(addIngredientBtn);
-
-        console.log(addIngredientBtn);
-    
-    }else if(category == 'r' && ingredientBtn == true){
-
-        ingredientBtn = false;
-        $("#add-ingredient").remove();
-
-    }else{
-
-    }
-
-});
-
-//ties ingredient add button to ingredientAdd() function.
-$(document).on("click", "#add-ingredient", function(event) {
-
-    event.preventDefault();
-
-    ingredientAdd();
-
-});
-    
 //constructs q= portion of queryURL for ingredient add category. 
 function ingredientAdd(){
     
@@ -232,6 +122,140 @@ function ingredientAdd(){
 
 }
 
+//email validation function
+function validateEmail(email) {
+    
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  
+}
+
+//Triggers search request, need to connect to ajax call.
+$(document).on("click", "#searchBtn", function(event) {
+
+    event.preventDefault();
+
+    //reset #recipe-list and searchedRecipeList array on new search
+    $("#recipe-list").html("");
+    searchedRecipeList = [];
+
+    var category = $(".category-input:checked").val();
+
+//if ingredient category selected and input box contains user input, will add to ingredient list if enter is pressed, constructs queryURL in proper format if enter and no input.    
+    if(category == 'i'){
+
+        if($("#table_filter").val() != ""){
+            
+            ingredientAdd();
+        
+        }else{
+            
+            queryURL = queryURL + q;
+
+            console.log("i search: " + queryURL);
+    
+        }
+
+//if recipe search constructs queryURL in proper format. 
+    }else if(category == 'r'){
+
+        
+        var recipe = $("#table_filter").val();
+        console.log(recipe);
+        var queryRecipe = "q=";
+
+        recipe = recipe.split(" ");
+
+        for(var i=0; i<recipe.length; i++){
+
+            if(i == 0){
+
+                queryRecipe += recipe[i];
+
+            }else{
+
+                queryRecipe += ("+" + recipe[i]);
+
+            }
+
+        }
+
+        queryURL = queryURL + queryRecipe;
+        $("#table_filter").val("");
+        
+        console.log(recipe);
+        console.log("r search: " + queryURL);
+    
+    }else{
+
+    };
+
+    //ajax call to return results for query
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var results = JSON.parse(response);
+        console.log(results);
+        $.each(results.recipes, function(index){
+            console.log(this.title);
+
+            var newDiv = $("<div>");
+            newDiv.addClass("recipe-select");
+            newDiv.attr("data-index", index);
+            newDiv.html(this.title);
+            $("#recipe-list").append(newDiv);
+
+            searchedRecipeList.push(this);
+        });
+    });
+
+    //reset queryURL
+    queryURL = "http://food2fork.com/api/search?key=ac439ce8f238ddbc8d1f8d5d4e74839a&";
+
+});
+
+//adds/removes ingredient add button based on selected category.
+$(document).on("click", ".category-input", function() {
+
+    var category = $(".category-input:checked").val();
+
+    console.log(category);
+
+    var addIngredientBtn = $("<button>");
+    addIngredientBtn.addClass("btn btn-light");
+    addIngredientBtn.attr("id", "add-ingredient");
+    addIngredientBtn.attr("form", "user-input");
+    addIngredientBtn.attr("type", "submit");
+    addIngredientBtn.text("Add");
+
+    if(category == 'i' && ingredientBtn == false){
+
+        ingredientBtn = true;
+        $("#dropdown-button").append(addIngredientBtn);
+
+        console.log(addIngredientBtn);
+    
+    }else if(category == 'r' && ingredientBtn == true){
+
+        ingredientBtn = false;
+        $("#add-ingredient").remove();
+
+    }else{
+
+    }
+
+});
+
+//ties ingredient add button to ingredientAdd() function.
+$(document).on("click", "#add-ingredient", function(event) {
+
+    event.preventDefault();
+
+    ingredientAdd();
+
+});
+
 //takes in input from login page, validates email address, displays modal if invalid input is provided.
 //Issues: minor: modal does not pause script execution so continuing to press enter runs click event code repeatedly, User would be unaffected.
 $(document).on("click", "#login-submit", function(event){
@@ -239,36 +263,35 @@ $(document).on("click", "#login-submit", function(event){
     event.preventDefault();
 
 
-        if($("#inputEmail").val().trim()){
+    if($("#inputEmail").val().trim()){
 
-            email = $("#inputEmail").val().trim();
+        email = $("#inputEmail").val().trim();
 
-            var valid = validateEmail(email);          
-            
-            if(valid == true){
+        var valid = validateEmail(email);          
+        
+        if(valid == true){
 
-            
-            }else{
-
-                email = null;
-
-                $("#modal-text").text("Please Enter a Valid Email.");
-            
-                $("#myModal").css("display", "block");
-
-                
-
-              
-            }
         
         }else{
 
-            $("#modal-text").text("Please Enter an Email Address.");
-            
+            email = null;
+
+            $("#modal-text").text("Please Enter a Valid Email.");
+        
             $("#myModal").css("display", "block");
- 
-         
+
+            
+
+            
         }
+        
+    }else{
+
+        $("#modal-text").text("Please Enter an Email Address.");
+        
+        $("#myModal").css("display", "block");
+ 
+    }
     
     if(email != null){
 
@@ -302,15 +325,7 @@ $(document).on("click", "#modal-close", function(){
 
 });
 
-//email validation function
-function validateEmail(email) {
-    
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  
-}
-    // database.ref().on("child_added", function(snapshot){
-    //     console.log(snapshot);
-    //     console.log(snapshot.val().id);
-    //     console.log(snapshot.val().recipeList);
-    // });
+$(document).on("click", ".recipe-select", function(){
+    $("#recipe-image").attr("src", searchedRecipeList[parseInt($(this).attr("data-index"))].image_url);
+});
+
